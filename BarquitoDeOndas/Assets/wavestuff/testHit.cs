d90e1f724846ecb4abe425426d243d6d091cdd4b
David Marull <david.marull@gmail.com>
2017-01-21 04:56:44 -0400

    Add wavestuff to the project
    cool shader wave effect by Eduardor

---
+++ BarquitoDeOndas/Assets/wavestuff/testHit.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testHit : MonoBehaviour {
+
+	public Camera cam;
+
+	float offWave;
+
+	//public GameObject goPlane;
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+		offWave = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hitInfo = new RaycastHit();
+			bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+			if (hit) 
+			{
+				this.GetComponent<Renderer> ().material.SetInt("_Click",1);
+				offWave = 1.0f;
+				this.GetComponent<Renderer> ().material.SetFloat ("_DirectionX",hitInfo.point.x);
+				this.GetComponent<Renderer> ().material.SetFloat ("_DirectionZ",hitInfo.point.z);
+				this.GetComponent<Renderer> ().material.SetFloat ("_OffWave",offWave);
+
+			} 
+
+		} 
+
+		offWave = offWave - (Time.fixedDeltaTime * 1.0f);
+
+		if (offWave < 0.0f)
+			offWave = 0.0f;
+
+		this.GetComponent<Renderer> ().material.SetFloat ("_OffWave",offWave);
+
+	}
+}
