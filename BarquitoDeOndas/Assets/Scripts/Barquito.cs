7f32262424094b75e8cea9ba368d42af23d36c68
David Marull <david.marull@gmail.com>
2017-01-21 17:07:33 -0400

    Delete unused code lines
                                    ,._
                           ,--.    |   `-.
                        ,-'    \   :      `-.
                       /__,.    \  ;  `--.___)
                      ,'    \    \/   /       ,-"`.
                         __,-' - /   '\      '   ,'
                      ,-'              `-._ ,---^.
                      \   ,                `-|    |
                       \,(o                  ;    |
                   _,-'   `-'                |    |
                ,-'                          |    |
            Y8PYF                            ;    ;
            `"" `           ,         ,--   /    :
             \      .   ___/|       ,'\   ,' ,'  ;
              `.     ;-' ___|     ,'  |\   ,'   /
                `---'  __\ /    ,'    | `-'   ,'
          ,-           \ ,'   ,'      `--.__,'
        ,'     ,-'     ,'    /
             ,'  ,     `----'    -hrr-
               ,'
              (
               `

--- BarquitoDeOndas/Assets/Scripts/Barquito.cs
+++ BarquitoDeOndas/Assets/Scripts/Barquito.cs
@@ -6,20 +6,12 @@ public class Barquito : MonoBehaviour {
 
     public static Barquito instance;
 
-    public static int life;
-
-    public float maxSpeed = 100;
-
     public Rigidbody rigid;
 
     void Start() {
         instance = this;
         rigid = this.GetComponent<Rigidbody>();
     }
-
-    //public void FixedUpdate() {
-    //    rigid.velocity = Vector3.ClampMagnitude( rigid.velocity, maxSpeed );
-    //}
 
     public void ReactToWave( MoveInfo moveInfo ) {
         rigid.AddForce( new Vector3( moveInfo.directionX, 0, moveInfo.directionZ ).normalized * (moveInfo.speed), ForceMode.Impulse );
