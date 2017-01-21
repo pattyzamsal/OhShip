1fd99e22f9948129cf64161cb9bdce60b82e50c2
David Marull <david.marull@gmail.com>
2017-01-21 19:39:34 -0400

    Create Buoys obstacle logic
             ,--._______,-.
           ,','  ,    .  ,_`-.
          / /  ,' , _` ``. |  )       `-..
         (,';'""`/ '"`-._ ` \/ ______    \\
           : ,o.-`- ,o.  )\` -'      `---.))
           : , d8b ^-.   '|   `.      `    `.
           |/ __:_     `. |  ,  `       `    \
           | ( ,-.`-.    ;'  ;   `       :    ;
           | |  ,   `.      /     ;      :    \
           ;-'`:::._,`.__),'             :     ;
          / ,  `-   `--                  ;     |
         /  \                   `       ,      |
        (    `     :              :    ,\      |
         \   `.    :     :        :  ,'  \    :
          \    `|-- `     \ ,'    ,-'     :-.-';
          :     |`--.______;     |        :    :
           :    /           |    |         |   \
           |    ;           ;    ;        /     ;
         _/--' |   -hrr-   :`-- /         \_:_:_|
       ,',','  |           |___ \
       `^._,--'           / , , .)
                          `-._,-'

---
+++ BarquitoDeOndas/Assets/Scripts/Buoys.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buoys : MonoBehaviour {
+
+    public MoveInfo moveInfo;
+
+    public float power;
+
+    void OnCollisionEnter( Collision coll ) {
+        if( coll.gameObject.tag == "OhShip" ) {
+            moveInfo = new MoveInfo();
+
+            moveInfo.speed = power;
+
+            moveInfo.directionX = coll.transform.position.x - this.transform.position.x;
+            moveInfo.directionY = coll.transform.position.y - this.transform.position.y;
+            moveInfo.directionZ = coll.transform.position.z - this.transform.position.z;
+
+            coll.gameObject.SendMessage( "ReactToWave", moveInfo );
+
+            GameManager.instance.LoseLife();
+        }
+    }
+}
