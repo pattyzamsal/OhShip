3f26b5afc196281dee7f8f2b5790fd8d3f16098c
Patricia Zambrano <pazs1991@gmail.com>
2017-01-22 02:14:06 -0400

    Create script to charge different levels

---
+++ BarquitoDeOndas/Assets/Scripts/ChargeLevels.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChargeLevels : MonoBehaviour {
+
+    public void SelectLevel(string level) {
+        SceneManager.LoadScene(level);
+    }
+}
