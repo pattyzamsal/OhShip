610d231952698d9801b500dc8673fa427f56d860
Patricia Zambrano <pazs1991@gmail.com>
2017-01-22 00:03:15 -0400

    Change name of the script

---
+++ BarquitoDeOndas/Assets/Scripts/ReviewCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReviewCollision : MonoBehaviour {
+
+    void OnTriggerEnter(Collider coll) {
+        if (coll.tag == "OhShip" && this.tag == "Obstacle") {
+            coll.gameObject.SendMessage("ActivateAnimation", true);
+
+            GameManager.instance.LoseLife();
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.tag == "Water" && this.tag == "Obstacle") {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+}
